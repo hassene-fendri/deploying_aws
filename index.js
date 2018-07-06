26b52b0569ecbf1437524f73b55b19dd55071cad
hasseneFendri <38699172+hasseneFendri@users.noreply.github.com>
2018-07-06 19:33:02 +0200

    index.js back to 1st version

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ app.get('/', (req, res) => {
   res.send('<h1>${process.env.MESSAGE}</h1>')
 })
 
-app.listen(8080,() => console.log('Server running on port 80'))
+app.listen(3000,() => console.log('Server running on port 80'))
